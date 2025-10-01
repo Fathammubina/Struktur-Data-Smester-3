3d7dd535e3b4d28da6b3a07c743c3686ca60b24c
Fathammubina <fathammubina15@gmail.com>
2025-10-01 23:47:09 +0700

    Add files via upload

---
+++ modul1/unguided1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float ang1, ang2;
+
+    cout << " input bilangan 1 : ";
+    cin >> ang1;
+    cout << " input bilangan 2 : ";
+    cin >> ang2;
+
+    cout << "penjumlahan = "<< ang1 + ang2 << endl;
+    cout << "pengurangan = "<< ang1 - ang2 << endl;
+    cout << "perkalian = "<< ang1 * ang2 << endl;
+    cout << "pembagian = "<< ang1 / ang2 << endl;
+    return 0;
+}
