d0df4ec4552806cde8bf8ef6f6a548ba735a94a1
Fathammubina <fathammubina15@gmail.com>
2026-01-06 13:20:41 +0700

    Add files via upload

---
+++ Modul 14/gmain.cpp
@@ -0,0 +1,33 @@
+#include"graf.h"
+#include "graf.cpp"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    Graph G;
+    createGraph(G);
+
+    // Tambah Node
+    InsertNode(G, 'A');
+    InsertNode(G, 'B');
+    InsertNode(G, 'C');
+    InsertNode(G, 'D');
+    InsertNode(G, 'E');
+    
+    // Hubungkan node (graph tak berarah)
+    ConnectNode(G, 'A', 'B');
+    ConnectNode(G, 'A', 'C');
+    ConnectNode(G, 'B', 'D');
+    ConnectNode(G, 'C', 'E');
+
+    cout << "=== Struktur Graph ===\n";
+    PrintInfoGraph(G);
+
+    cout << "\n=== DFS dari Node A ===\n";
+    ResetVisited(G);
+    PrintDFS(G, FindNode(G, 'A'));
+
+    cout << endl;
+    return 0;
+}
